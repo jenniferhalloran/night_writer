4be1d5eb96aeaeaee43356e957e1ae1e278629d5
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-04-21 18:21:37 -0400

    Create nightwriter class

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,1 +1,11 @@
+class NightWriter
+  attr_reader :existing_file,
+              :new_file
 
+  def initialize()
+    @existing_file = "message.txt"#ARGV[0]
+    @new_file = "braille.txt" #ARGV[1]
+    ##may be good to have other default files if something is not entered.
+  end
+
+end
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,1 +1,11 @@
 require './spec/spec_helper'
+
+RSpec.describe NightWriter do
+  let(:nightwriter) {NightWriter.new}
+
+  it "exists" do
+    ##do I have to mock/stub?? Merp
+    expect(nightwriter).to be_a(NightWriter)
+  end
+
+end
