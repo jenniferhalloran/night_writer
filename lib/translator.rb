204fe1662b32241d43ce8b99c33b18b35576df91
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-04-25 21:35:02 -0400

    Clean up translator class

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,11 @@
 class Translator
-  attr_reader :braille_equivalent
 
-  def initialize
-    @braille_equivalent = {
+#   def initialize
+    # can I not make braille_translations an attribute to be accessed?
+#   end
+
+  def braille_translations
+    {
       "a" => ["O.", "..", ".."],
       "b" => ["O.", "O.", ".."],
       "c" => ["OO", "..", ".."],
@@ -31,60 +34,14 @@ class Translator
       "z" => ["O.", ".O", "OO"],
       " " => ["..", "..", ".."]
     }
-    @english_equivalent = @braille_equivalent.invert
-    @braille_message = []
   end
-##translator class
+
   def translate_letter(letter, dictionary)
     dictionary[letter]
   end
 
   def translate_message(message, dictionary)
     message.map {|letter| translate_letter(letter, dictionary)}
-  end
-
-######
-  def english_to_braille(english_string)
-    english_characters = breakdown_message(english_string)
-    braille_letters = translate_message(english_characters, @braille_equivalent)
-    format_braille_letters(braille_letters)
-  end
-
-  def breakdown_message(english_string)
-    english_string.chomp.chars
-  end
-
-  def format_braille_letters(braille_letters)
-    braille_lines = braille_letters.transpose
-    create_line_breaks(braille_lines)
-    @braille_message.flatten.join
-  end
-
-  def create_line_breaks(braille_lines)
-    index = 0
-    (braille_lines[0].count.to_f / 40).ceil.times do
-      braille_lines.each do |line|
-        @braille_message << line[index..index + 39]
-        @braille_message << "\n"
-      end
-      index += 40
-    end
-  end
-
-## BRAILLE TO ENGLISH
-  def braille_to_english(braille_message)
-    braille_letters = format_braille_for_translation(braille_message)
-    english_array = translate_braille_letters(braille_letters)
-    english_array.join
-  end
-
-  def format_braille_for_translation(braille_message)
-    braille_arrays = braille_message.split("\n").map {|line| line.scan(/../)}.each_slice(3)
-    braille_arrays.map{|array| array.transpose}
-  end
-
-  def translate_braille_letters(braille_letters)
-    braille_letters.map { |braille_letter| translate_message(braille_letter, @english_equivalent) }
   end
 
 end
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -2,79 +2,24 @@ require './lib/translator.rb'
 
 RSpec.describe Translator do
   let(:translator) {Translator.new}
+  let(:braille_translator) {BrailleTranslator.new}
 
 
   it "exists" do
     expect(translator).to be_a(Translator)
   end
 
-  it "has readable attributes" do
-    expected_keys = ["a", "b", "c", "d", "e", "f", "g",
-                         "h", "i", "j", "k", "l", "m", "n",
-                         "o", "p", "q", "r", "s", "t", "u",
-                         "v", "w", "x", "y", "z", " "]
-
-    expect(translator.braille_equivalent.keys).to eq(expected_keys)
+  it "can translate braille and english" do
+    expect(translator.translate_letter("a", translator.braille_translations)).to eq(["O.", "..", ".."])
+    expect(translator.translate_letter(["O.", "..", ".."], braille_translator.english_translations)).to eq("a")
   end
 
-  it "can break given english string into characters" do
-    expect(translator.breakdown_message("hello")).to eq(["h", "e", "l", "l", "o"])
-  end
-
-  it "can translate an individual character" do
-    expect(translator.translate_letter("h", translator.braille_equivalent)).to eq(["O.", "OO", ".."])
-    expect(translator.translate_letter(" ", translator.braille_equivalent)).to eq(["..", "..", ".."])
-
-  end
-  it "can translate the broken up english string to unformatted braille" do
-    expected_unformatted_braille = [["O.", "OO", ".."], ["O.", ".O", ".."],
-                                    ["O.", "O.", "O."], ["O.", "O.", "O."],
-                                    ["O.", ".O", "O."]]
-
-    expect(translator.translate_message(["h", "e", "l", "l", "o"], translator.braille_equivalent)).to eq(expected_unformatted_braille)
-  end
-
-  it "can format braille letters into printable braille in three rows of two characters" do
-      unformatted_braille = [["O.", "OO", ".."], ["O.", ".O", ".."],
-                             ["O.", "O.", "O."], ["O.", "O.", "O."],
-                             ["O.", ".O", "O."]]
-    expect(translator.format_braille_letters(unformatted_braille)).to eq("O.O.O.O.O.\nOO.OO.O..O\n....O.O.O.\n")
-  end
-
-  it "can split messages over 40 braille characters onto multiple lines" do
-    printable_braille = translator.english_to_braille("hello this is going to be just long enough")
-    expect(printable_braille).to eq("O.O.O.O.O....OO..O.O...O.O..OOO..OOOOO...OO...O.O....OO..O.O..O.O.OOOO..O.OOO.O.\n" +
-                                     "OO.OO.O..O..OOOOO.O...O.O...OO.OO..OOO..OO.O..O..O..OO..O.OO..O..O.OOO...O.O.O..\n" +
-                                     "....O.O.O...O.....O.....O.....O...O.....O.O...........OOO.O...O.O.O.......O.O.OO\n" +
-                                     "OOO.\n" +
-                                     "OOOO\n" +
-                                     "....\n")
-  end
-
-  it "can format a braille message for translation" do
-    unformatted_braille = "O.O.O.O.O.\nOO.OO.O..O\n....O.O.O.\n"
-    expected_braille_letters =[[["O.", "OO", ".."],
-                                ["O.", ".O", ".."],
-                                ["O.", "O.", "O."],
-                                ["O.", "O.", "O."],
-                                ["O.", ".O", "O."]]]
-
-    expect(translator.format_braille_for_translation(unformatted_braille)).to eq(expected_braille_letters)
-  end
-
-  it "can translate braille letters back to english " do
-    braille_letters = [[["O.", "OO", ".."],
-                        ["O.", ".O", ".."],
-                        ["O.", "O.", "O."],
-                        ["O.", "O.", "O."],
-                        ["O.", ".O", "O."]]]
-    expect(translator.translate_braille_letters(braille_letters)).to eq([["h", "e", "l", "l", "o"]])
-  end
-
-  it "can create a printable english message from braille" do
-    unformatted_braille = "O.O.O.O.O.\nOO.OO.O..O\n....O.O.O.\n"
-
-    expect(translator.braille_to_english(unformatted_braille)).to eq("hello")
+  it "can translate braille and english messages" do
+    hello_in_braille = [["O.", "OO", ".."], ["O.", ".O", ".."],
+                        ["O.", "O.", "O."], ["O.", "O.", "O."],
+                        ["O.", ".O", "O."]]
+    expect(translator.translate_message(["h", "e", "l", "l", "o"], translator.braille_translations)).to eq(hello_in_braille)
+    expect(translator.translate_message(hello_in_braille, braille_translator.english_translations)).to eq(["h", "e", "l", "l", "o"])
   end
 
 end
