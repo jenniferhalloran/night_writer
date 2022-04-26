4931c752147bf8b1beb2a8e771bb482e2e246e21
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-04-25 21:09:47 -0400

    Add methods and tests to english_translator

--- lib/english_translator.rb
+++ lib/english_translator.rb
@@ -2,5 +2,35 @@ require './lib/translator'
 
 class EnglishTranslator < Translator
 
+  def initialize
+    @braille_message = []
+  end
+
+  def english_to_braille(english_string)
+    english_characters = breakdown_message(english_string)
+    braille_letters = translate_message(english_characters, braille_translations)
+    format_braille_letters(braille_letters)
+  end
+
+  def breakdown_message(english_string)
+    english_string.chomp.chars
+  end
+
+  def format_braille_letters(braille_letters)
+    braille_lines = braille_letters.transpose
+    create_line_breaks(braille_lines)
+    @braille_message.flatten.join
+  end
+
+  def create_line_breaks(braille_lines)
+    index = 0
+    (braille_lines[0].count.to_f / 40).ceil.times do
+      braille_lines.each do |line|
+        @braille_message << line[index..index + 39]
+        @braille_message << "\n"
+      end
+      index += 40
+    end
+  end
 
 end
--- spec/english_translator_spec.rb
+++ spec/english_translator_spec.rb
@@ -6,18 +6,47 @@ RSpec.describe Translator do
   it "exists" do
     expect(english_translator).to be_a(EnglishTranslator)
   end
-end
 
+  it "can access the translations" do
+    expected_keys = ["a", "b", "c", "d", "e", "f", "g",
+                         "h", "i", "j", "k", "l", "m", "n",
+                         "o", "p", "q", "r", "s", "t", "u",
+                         "v", "w", "x", "y", "z", " "]
 
-def translate_braille_string(braille_letters)
-  braille_letters.map { |braille_letter| translate_letter(braille_letter, @english_equivalent) }
-end
+    expect(english_translator.braille_translations.keys).to eq(expected_keys)
+  end
 
-def translate_string_breakdown(english_string)
-  message_characters = breakdown_message(english_string)
-  message_characters.map { |english_letter| translate_letter(english_letter, @braille_equivalent) }
-end
+  it "can break given english string into characters" do
+    expect(english_translator.breakdown_message("hello")).to eq(["h", "e", "l", "l", "o"])
+  end
 
-def translate_message(message, dictionary)
-  message.map {|letter| translate_letter(letter, dictionary)}
+  it "can translate an individual character" do
+    expect(english_translator.translate_letter("h", english_translator.braille_translations)).to eq(["O.", "OO", ".."])
+    expect(english_translator.translate_letter(" ", english_translator.braille_translations)).to eq(["..", "..", ".."])
+
+  end
+  it "can translate the broken up english string to unformatted braille" do
+    expected_unformatted_braille = [["O.", "OO", ".."], ["O.", ".O", ".."],
+                                    ["O.", "O.", "O."], ["O.", "O.", "O."],
+                                    ["O.", ".O", "O."]]
+
+    expect(english_translator.translate_message(["h", "e", "l", "l", "o"], english_translator.braille_translations)).to eq(expected_unformatted_braille)
+  end
+
+  it "can format braille letters into printable braille in three rows of two characters" do
+      unformatted_braille = [["O.", "OO", ".."], ["O.", ".O", ".."],
+                             ["O.", "O.", "O."], ["O.", "O.", "O."],
+                             ["O.", ".O", "O."]]
+    expect(english_translator.format_braille_letters(unformatted_braille)).to eq("O.O.O.O.O.\nOO.OO.O..O\n....O.O.O.\n")
+  end
+
+  it "can split messages over 40 braille characters onto multiple lines" do
+    printable_braille = english_translator.english_to_braille("hello this is going to be just long enough")
+    expect(printable_braille).to eq("O.O.O.O.O....OO..O.O...O.O..OOO..OOOOO...OO...O.O....OO..O.O..O.O.OOOO..O.OOO.O.\n" +
+                                     "OO.OO.O..O..OOOOO.O...O.O...OO.OO..OOO..OO.O..O..O..OO..O.OO..O..O.OOO...O.O.O..\n" +
+                                     "....O.O.O...O.....O.....O.....O...O.....O.O...........OOO.O...O.O.O.......O.O.OO\n" +
+                                     "OOO.\n" +
+                                     "OOOO\n" +
+                                     "....\n")
+  end
 end
